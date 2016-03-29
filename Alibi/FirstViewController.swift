d316e8e52158194f9b4064ccc63f59b9a7bcb2cd
JackY <wo131485508@qq.com>
2016-03-29 00:13:10 -0400

    Initial State

---
+++ Alibi/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Alibi
+//
+//  Created by Yaxin Yuan on 16/3/29.
+//  Copyright © 2016年 Yaxin Yuan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
