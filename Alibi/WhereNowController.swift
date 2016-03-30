f525d29a67d8f2633859a68a404eb7b61e72b132
JackY <wo131485508@qq.com>
2016-03-29 21:35:39 -0400

    GetCurrentLocation

---
+++ Alibi/WhereNowController.swift
@@ -0,0 +1,94 @@
+//
+//  FirstViewController.swift
+//  Alibi
+//
+//  Created by Yaxin Yuan on 16/3/29.
+//  Copyright © 2016年 Yaxin Yuan. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class WhereNowController: UIViewController, CLLocationManagerDelegate {
+    
+    var location: CLLocation?
+    var placemark: CLPlacemark?
+    
+    
+    let manager = CLLocationManager()
+    let geoCoder = CLGeocoder()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func getCurrentLocation(){
+        let authStatus = CLLocationManager.authorizationStatus()
+        
+        if authStatus == .NotDetermined {
+            manager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.last!
+        geoCoder.reverseGeocodeLocation(location!){
+            placemarks, _ in
+            if let p = placemarks where !p.isEmpty{
+                self.placemark = p.last
+                self.manager.delegate = nil
+                self.manager.stopUpdatingLocation()
+                let currentLocation = self.stringFromPlacemark(self.placemark!)
+                let alert = UIAlertController(title: "Curren Location", message: currentLocation, preferredStyle: .Alert)
+                let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                alert.addAction(action)
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error")
+    }
+    
+    func stringFromPlacemark(placemark: CLPlacemark) -> String{
+        var line1 = ""
+        
+        if let s = placemark.subThoroughfare {
+            line1 += s + " "
+        }
+        if let s = placemark.thoroughfare {
+            line1 += s
+        }
+        
+        var line2 = ""
+        
+        if let s = placemark.locality {
+            line2 += s + " "
+        }
+        if let s = placemark.administrativeArea {
+            line2 += s + " "
+        }
+        if let s = placemark.postalCode {
+            line2 += s
+        }
+        
+        return line1 + "\n" + line2
+    }
+    
+        
+}
+    
+    
+    
+    
+
+
+
+
+
