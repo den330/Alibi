2d307dff52e30d8104988d7836d44539aa58ad67
JackY <wo131485508@qq.com>
2016-04-01 15:03:40 -0400

    Minor Update

--- Alibi/LocationHistoryController.swift
+++ Alibi/LocationHistoryController.swift
@@ -50,13 +50,7 @@ class LocationHistoryController: UITableViewController {
             return
         }
     }
-    
-    deinit {
-        fetchedResultsController.delegate = nil
-    }
-    
 
-    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let sectionInfo = fetchedResultsController.sections![section]
         return sectionInfo.numberOfObjects
@@ -93,35 +87,32 @@ class LocationHistoryController: UITableViewController {
 extension LocationHistoryController: NSFetchedResultsControllerDelegate{
     
     func controllerWillChangeContent(controller: NSFetchedResultsController) {
-        print("*** controllerWillChangeContent")
         tableView.beginUpdates()
     }
     
     func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
         switch type {
+        
         case .Insert:
-            print("*** NSFetchedResultsChangeInsert (object)")
+            
             tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
             
         case .Delete:
-            print("*** NSFetchedResultsChangeDelete (object)")
+            
             tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
             
         case .Update:
-            print("*** NSFetchedResultsChangeUpdate (object)")
+            
             if let cell = tableView.cellForRowAtIndexPath(indexPath!) as? LocationCell {
                 let location = controller.objectAtIndexPath(indexPath!) as! Location
                 cell.configureForLocation(location)
             }
-        case .Move:
-            print("*** NSFetchedResultsChangeMove (object)")
-            tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
-            tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+            
+        default: break
         }
     }
     
     func controllerDidChangeContent(controller: NSFetchedResultsController) {
-        print("*** controllerDidChangeContent")
         tableView.endUpdates()
     }
 }
